9863b2ed278f6a2d82743c505e0f4921b73f9de1
Abhishek <ak723215@gmail.com>
2022-02-07 15:54:32 +0530

    Initial commit

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,32 @@
+import {React } from 'react'
+import {useState, useEffect} from 'react'
+import Header from '../../component/header/Header'
+import Posts from '../../component/posts/Posts'
+import Sidebar from '../../component/sidebar/Sidebar'
+import './home.css'
+import axios from 'axios'
+import {useLocation} from 'react-router-dom'
+
+
+export default function Home() {
+    const [posts, setPosts] = useState([]);
+    const {search} = useLocation();
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+           const res = await axios.get("/posts" + search)
+        setPosts(res.data)
+        }
+        fetchPosts();
+    },[search])
+    return (
+        <>
+        <Header />
+        <div className='home'>
+            <Posts posts={posts}/>
+            <Sidebar />
+            
+        </div>
+        </>
+    );
+}
